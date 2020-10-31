803f9235449095c834b4523fbe1dcd501a90f682
Valeriy Van <github@w7software.com>
2020-11-01 00:30:09 +0100

    Moves Rgb565 into extension

--- Sources/SenseHat/SenseHat.swift
+++ Sources/SenseHat/SenseHat.swift
@@ -13,70 +13,6 @@ public class SenseHat {
     // take respectively 5, 6 and 5 bits.
     public struct Rgb565 {
         var value: UInt16
-
-        var red: UInt8 {
-            get {
-                UInt8(truncatingIfNeeded: value >> 11)
-            }
-            set(newValue) {
-                value = (value & 0b0000_0111_1111_1111) | (UInt16(newValue) << 11)
-            }
-        }
-
-        var green: UInt8 {
-            get {
-                UInt8(truncatingIfNeeded: (value & 0b0000_0111_1110_0000) >> 5)
-            }
-            set(newValue) {
-                value = (value & 0b1111_1000_0001_1111) | ((UInt16(newValue) & 0b0011_1111) << 5)
-            }
-        }
-
-        var blue: UInt8 {
-            get {
-                UInt8(truncatingIfNeeded: value) & 0b1_1111
-            }
-            set(newValue) {
-                value = (value & 0b1111_1111_1110_0000) | (UInt16(newValue) & 0b0001_1111)
-            }
-        }
-
-        init(value: UInt16) {
-            self.value = value
-        }
-
-        init(red: UInt8, green: UInt8, blue: UInt8) {
-            value = (UInt16(red) << 11) | ((UInt16(green) & 0b0011_1111) << 5) | (UInt16(blue) & 0b0001_1111)
-        }
-
-        public static var red: Rgb565
-            { Rgb565(value: 0b1111_1000_0000_0000) }
-        public static var green: Rgb565
-            { Rgb565(value: 0b0000_0111_1110_0000) }
-        public static var blue: Rgb565
-            { Rgb565(value: 0b0000_0000_0001_1111) }
-        public static var white: Rgb565
-            { Rgb565(value: 0b1111_1111_1111_1111) }
-        public static var black: Rgb565
-            { Rgb565(value: 0b0000_0000_0000_0000) }
-        public static var brown: Rgb565
-            { Rgb565(value: 0b1001_1011_0010_0110) } // R:0.6, G:0.4, B:0.2
-        public static var cyan: Rgb565
-            { Rgb565(value: 0b0000_0111_1111_1111) } // R:0.0, G:1.0, B:1.0
-        public static var magenta: Rgb565
-            { Rgb565(value: 0b1111_1000_0001_1111) } // R:1.0, G:0.0, B:1.0
-        public static var yellow: Rgb565
-            { Rgb565(value: 0b1111_1111_1110_0000) } // R:1.0, G:1.0, B:0.0
-        public static var purple: Rgb565
-            { Rgb565(value: 0b1000_0000_0001_0000) } // R:0.5, G:0.0, B:0.5
-        public static var orange: Rgb565
-            { Rgb565(value: 0b1111_1100_0000_0000) } // R:1.0, G:0.5, B:0.0
-        public static var gray: Rgb565
-            { Rgb565(value: 0b1000_0100_0001_0000) } // R:0.5, G:0.5, B:0.5
-        public static var lightGray: Rgb565
-            { Rgb565(value: 0b1010_1101_0101_0101) } // R:2/3, G:2/3, B:2/3
-        public static var darkGray: Rgb565
-            { Rgb565(value: 0b0101_0010_1010_1010) } // R:1/3, G:1/3, B:1/3
     }
 
     private var fileDescriptor: Int32
@@ -121,6 +57,68 @@ public class SenseHat {
             frameBuffer.advanced(by: i*2).storeBytes(of: color, as: Rgb565.self)
         }
     }
+}
+
+extension SenseHat.Rgb565 {
+    var red: UInt8 {
+        get {
+            UInt8(truncatingIfNeeded: value >> 11)
+        }
+        set(newValue) {
+            value = (value & 0b0000_0111_1111_1111) | (UInt16(newValue) << 11)
+        }
+    }
+
+    var green: UInt8 {
+        get {
+            UInt8(truncatingIfNeeded: (value & 0b0000_0111_1110_0000) >> 5)
+        }
+        set(newValue) {
+            value = (value & 0b1111_1000_0001_1111) | ((UInt16(newValue) & 0b0011_1111) << 5)
+        }
+    }
+
+    var blue: UInt8 {
+        get {
+            UInt8(truncatingIfNeeded: value) & 0b1_1111
+        }
+        set(newValue) {
+            value = (value & 0b1111_1111_1110_0000) | (UInt16(newValue) & 0b0001_1111)
+        }
+    }
+
+    init(red: UInt8, green: UInt8, blue: UInt8) {
+        value = (UInt16(red) << 11) | ((UInt16(green) & 0b0011_1111) << 5) | (UInt16(blue) & 0b0001_1111)
+    }
+
+    public static var red: SenseHat.Rgb565
+        { SenseHat.Rgb565(value: 0b1111_1000_0000_0000) }
+    public static var green: SenseHat.Rgb565
+        { SenseHat.Rgb565(value: 0b0000_0111_1110_0000) }
+    public static var blue: SenseHat.Rgb565
+        { SenseHat.Rgb565(value: 0b0000_0000_0001_1111) }
+    public static var white: SenseHat.Rgb565
+        { SenseHat.Rgb565(value: 0b1111_1111_1111_1111) }
+    public static var black: SenseHat.Rgb565
+        { SenseHat.Rgb565(value: 0b0000_0000_0000_0000) }
+    public static var brown: SenseHat.Rgb565
+        { SenseHat.Rgb565(value: 0b1001_1011_0010_0110) } // R:0.6, G:0.4, B:0.2
+    public static var cyan: SenseHat.Rgb565
+        { SenseHat.Rgb565(value: 0b0000_0111_1111_1111) } // R:0.0, G:1.0, B:1.0
+    public static var magenta: SenseHat.Rgb565
+        { SenseHat.Rgb565(value: 0b1111_1000_0001_1111) } // R:1.0, G:0.0, B:1.0
+    public static var yellow: SenseHat.Rgb565
+        { SenseHat.Rgb565(value: 0b1111_1111_1110_0000) } // R:1.0, G:1.0, B:0.0
+    public static var purple: SenseHat.Rgb565
+        { SenseHat.Rgb565(value: 0b1000_0000_0001_0000) } // R:0.5, G:0.0, B:0.5
+    public static var orange: SenseHat.Rgb565
+        { SenseHat.Rgb565(value: 0b1111_1100_0000_0000) } // R:1.0, G:0.5, B:0.0
+    public static var gray: SenseHat.Rgb565
+        { SenseHat.Rgb565(value: 0b1000_0100_0001_0000) } // R:0.5, G:0.5, B:0.5
+    public static var lightGray: SenseHat.Rgb565
+        { SenseHat.Rgb565(value: 0b1010_1101_0101_0101) } // R:2/3, G:2/3, B:2/3
+    public static var darkGray: SenseHat.Rgb565
+        { SenseHat.Rgb565(value: 0b0101_0010_1010_1010) } // R:1/3, G:1/3, B:1/3
 }
 
 // MARK: - Darwin / Xcode Support
