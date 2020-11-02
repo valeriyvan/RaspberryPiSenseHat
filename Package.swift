28742b8fae661e2d5449b8d68408f996fc7b2296
Valeriy Van <github@w7software.com>
2020-11-02 16:05:21 +0100

    Change formatting in package manifest

--- Package.swift
+++ Package.swift
@@ -5,27 +5,15 @@ import PackageDescription
 let package = Package(
     name: "SenseHat",
     products: [
-        .library(
-            name: "SenseHat",
-            targets: ["SenseHat"]),
-        .library(
-            name: "Font8x8",
-            targets: ["Font8x8"]),
+        .library(name: "SenseHat", targets: ["SenseHat"]),
+        .library(name: "Font8x8", targets: ["Font8x8"]),
         .executable(name: "Blink", targets: ["Blink"])
     ],
     dependencies: [],
     targets: [
-        .target(
-            name: "SenseHat",
-            dependencies: ["Font8x8"]),
-        .target(
-            name: "Font8x8",
-            dependencies: []),
-        .target(
-            name: "Blink",
-            dependencies: ["SenseHat"]),
-        .testTarget(
-            name: "SenseHatTests",
-            dependencies: ["SenseHat"]),
+        .target(name: "SenseHat", dependencies: ["Font8x8"]),
+        .target(name: "Font8x8", dependencies: []),
+        .target(name: "Blink", dependencies: ["SenseHat"]),
+        .testTarget(name: "SenseHatTests", dependencies: ["SenseHat"]),
     ]
 )
