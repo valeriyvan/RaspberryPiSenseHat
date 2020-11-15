d2871f2cde5b19599c89d4cdea49245d2aefaa09
Valeriy Van <github@w7software.com>
2020-11-15 19:50:02 +0100

    Make O_SYNC available on any platform

--- Sources/SenseHat/SenseHat.swift
+++ Sources/SenseHat/SenseHat.swift
@@ -688,5 +688,5 @@ extension SenseHat {
 
 // MARK: Darwin / Xcode Support
 #if os(OSX) || os(iOS)
-private var O_SYNC: CInt { fatalError("Linux only") }
+private var O_SYNC: CInt {0} //{ fatalError("Linux only") }
 #endif
