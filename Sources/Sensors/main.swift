88f3a1b9b61cb8dbc677d1fcaafcda433ab94959
Valeriy Van <github@w7software.com>
2020-11-22 11:41:07 +0100

    Update Sensors target for reading pressure

--- Sources/Sensors/main.swift
+++ Sources/Sensors/main.swift
@@ -16,11 +16,30 @@ guard let senseHat = SenseHat() else {
     fatalError("Can't initialise Raspberry Pi Sense Hat")
 }
 
+print("Started")
+
 for _ in 0..<1000 {
+    print("Reading humidity sensor")
     let h = senseHat.ambientHumidity()!
-    let str = String(format: "%.1lf", h.H_rH)
-    let msg = "Humidity \(str)% rH "
-    print(msg)
-    senseHat.show(string: msg, secPerChar: 0.5, color: .yellow, background: .black)
+    let strH = String(format: "%.1lf", h.H_rH)
+    let strT = String(format: "%.1lf", h.T_DegC)
+    let msgH = "Humidity \(strH)% rH "
+    let msgT = "Temperature \(strT)ºC"
+    print(msgH)
+    print(msgT)
+    senseHat.show(string: msgH, secPerChar: 0.5, color: .yellow, background: .black)
+    print()
+    sleep(2)
+
+    print("Reading pressure sensor")
+    let p = senseHat.pressure()!
+    let strP = String(format: "%.1lf", p.P_hPa)
+    let strTp = String(format: "%.1lf", p.T_DegC)
+    let msgP = "Pressure \(strP) hPa "
+    let msgTp = "Temperature \(strTp)ºC"
+    print(msgP)
+    print(msgTp)
+    senseHat.show(string: msgP, secPerChar: 0.5, color: .yellow, background: .black)
+    print()
     sleep(10)
 }
