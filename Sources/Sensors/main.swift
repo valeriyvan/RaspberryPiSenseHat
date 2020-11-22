ea42af343030a4f542ff034637e828885614bdf0
Valeriy Van <github@w7software.com>
2020-11-22 19:03:43 +0100

    Refactor Sensor's target main

--- Sources/Sensors/main.swift
+++ Sources/Sensors/main.swift
@@ -18,28 +18,34 @@ guard let senseHat = SenseHat() else {
 
 print("Started")
 
-for _ in 0..<1000 {
+for _ in 0..<100 {
     print("Reading humidity sensor")
-    let h = senseHat.humidity()!
-    let strH = String(format: "%.1lf", h.H_rH)
-    let strT = String(format: "%.1lf", h.T_DegC)
-    let msgH = "Humidity \(strH)% rH "
-    let msgT = "Temperature \(strT)ºC"
-    print(msgH)
-    print(msgT)
-    senseHat.show(string: msgH, secPerChar: 0.5, color: .yellow, background: .black)
-    print()
+    if let h = senseHat.humidity() {
+        let strH = String(format: "%.1lf", h.H_rH)
+        let strT = String(format: "%.1lf", h.T_DegC)
+        let msgH = "Humidity \(strH)% rH "
+        let msgT = "Temperature \(strT)ºC"
+        print(msgH)
+        print(msgT)
+        senseHat.show(string: msgH, secPerChar: 0.5, color: .yellow, background: .black)
+    } else {
+        print("Cannot read humidity sensor")
+    }
+    print("Waiting 2 seconds\n")
     sleep(2)
 
     print("Reading pressure sensor")
-    let p = senseHat.pressure()!
-    let strP = String(format: "%.1lf", p.P_hPa)
-    let strTp = String(format: "%.1lf", p.T_DegC)
-    let msgP = "Pressure \(strP) hPa "
-    let msgTp = "Temperature \(strTp)ºC"
-    print(msgP)
-    print(msgTp)
-    senseHat.show(string: msgP, secPerChar: 0.5, color: .yellow, background: .black)
-    print()
-    sleep(10)
+    if let p = senseHat.pressure() {
+        let strP = String(format: "%.1lf", p.P_hPa)
+        let strTp = String(format: "%.1lf", p.T_DegC)
+        let msgP = "Pressure \(strP) hPa "
+        let msgTp = "Temperature \(strTp)ºC"
+        print(msgP)
+        print(msgTp)
+        senseHat.show(string: msgP, secPerChar: 0.5, color: .yellow, background: .black)
+    } else {
+        print("Cannot read pressure sensor")
+    }
+    print("Waiting 2 seconds\n")
+    sleep(2)
 }
